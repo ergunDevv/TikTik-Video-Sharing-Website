285a1850a3e278c7e8207d90b980359a849a7698
ergunDevv <ergncyln2127088@gmail.com>
2023-01-16 18:02:14 +0300

    Frontend components created,tailwind and css added to the project

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import {AiOutlineLogout} from 'react-icons/ai'
+import {BiSearch} from 'react-icons/bi'
+import {IoMdAdd} from 'react-icons/io'
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+      
+
+    </div>
+  )
+}
+
+export default Navbar
