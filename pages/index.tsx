4b1348fe8b213b9867db0577485d79e195687793
ergunDevv <ergncyln2127088@gmail.com>
2023-01-18 17:07:11 +0300

    VideoCard, NoResults components added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,40 @@
-export default function Home() {
-  return (
-    <>
+import type { NextPage } from "next"
+import axios from "axios"
 
-      <main >
-        <div >
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-       </h1>
-        </div>
-      </main>
-    </>
+import { Video } from "../types"
+import NoResults from "../components/NoResults"
+import VideoCard from "../components/VideoCard"
+
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({videos}: IProps) => {
+  console.log(videos)
+  return (
+    <div className="flex flex-col gap-10 videos h-full"> 
+      {videos.length ? (
+        videos.map((video: Video)=>(
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
   )
 }
+
+
+export const getServerSideProps = async ()=> {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+
+  
+  return {
+    props: {
+      videos:data
+    }
+  }
+}
+
+
+export default Home
