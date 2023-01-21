11e59c11b330c7958a3b8b99f07521152f402cc2
ergunDevv <ergncyln2127088@gmail.com>
2023-01-21 16:19:49 +0300

    Video detail,Profile details added

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -71,12 +71,12 @@ const VideoCard: NextPage<IProps> = ({post}) => {
             onMouseEnter={()=>setIsHover(true)}
             onMouseLeave={()=>setIsHover(false)}
             className='rounded-3xl'>
-              <Link href='/'>
+              <Link href={`/detail/${post._id}`}>
                 <video 
                 src={post.video.asset.url}
                 ref={videoRef}
                 loop
-                className='lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+                className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
                 > 
 
                 </video>
