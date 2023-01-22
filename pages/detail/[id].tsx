1ce7f0a00b6f51de0ac4f9f0e1c223c904466787
ergunDevv <ergncyln2127088@gmail.com>
2023-01-22 15:11:55 +0300

    Video added on video detail page with play and pause prop

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -10,12 +10,59 @@ import axios from 'axios'
 
 
 import { BASE_URL } from '../../utils'
+import {Video} from '../../types'
+
+interface IProps {
+  postDetails :Video,
+
+}
+
+const Detail = ({postDetails}:IProps) => {
+  const [post,setPost] = useState(postDetails)
+  const [playing,setPlaying] = useState(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoClick= ()=>{
+    if(playing){
+      videoRef?.current?.pause();
+      setPlaying(false);
+    }else{
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  }
 
 
-const Detail = () => {
+  if(!post) return null;
+
   return (
-    <div>
-
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black bg-no-repeat bg-cover bg-center'>
+        <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+          <p>
+            <MdOutlineCancel className='text-white text-[35px]'/>
+          </p>
+        </div>
+        <div className='relative'>
+          <div className='lg:h-[100vh] h-[60vh]'>
+              <video 
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+              ref={videoRef}
+              loop
+              onClick={onVideoClick}
+              ></video>
+          </div>
+          <div className='absolute top-[45%] left-[45%] cursor-pointer'>
+            {!playing && ( 
+              <button onClick={onVideoClick}>
+                <BsFillPlayFill className='text-white text-6xl lg:text-8xl'/>
+              </button>
+            )} 
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
