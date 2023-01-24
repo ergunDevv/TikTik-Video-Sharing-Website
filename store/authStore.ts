1dbde8df1543389f72c24e18a0d595297cc53679
ergunDevv <ergncyln2127088@gmail.com>
2023-01-24 16:52:01 +0300

    Comment component front-end and functionality added

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,18 +1,27 @@
-import create from 'zustand'
-import {persist} from 'zustand/middleware'
-import axios from 'axios'
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import axios from 'axios';
 
-const authStore = (set: any) =>({
-    userProfile:null,
+import { BASE_URL } from '../utils';
 
-    addUser:(user: any)=> set({userProfile:user}),
-    removeUser: () => set({ userProfile : null})
+const authStore = (set: any) => ({
+  userProfile: null,
+  allUsers: [],
+  
+  addUser: (user: any) => set({ userProfile: user }),
+  removeUser: () => set({ userProfile: null }),
+
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+
+    set({ allUsers: response.data });
+  },
 });
 
-const useAuthStore = create(
-    persist(authStore,{
-        name: 'auth'
-    })
-)
+const useAuthStore = create((
+  persist(authStore, {
+    name: 'auth',
+  })
+));
 
 export default useAuthStore;
