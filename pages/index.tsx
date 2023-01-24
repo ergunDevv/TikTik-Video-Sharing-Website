08b4b0dd236d8679ac2601cd7f528aefc9d57819
ergunDevv <ergncyln2127088@gmail.com>
2023-01-25 00:01:41 +0300

    Search component added and final version of tiktik

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,41 @@
-import type { NextPage } from "next"
-import axios from "axios"
+import React from 'react';
+import axios from 'axios';
 
-import { Video } from "../types"
-import NoResults from "../components/NoResults"
-import VideoCard from "../components/VideoCard"
-import { BASE_URL } from "../utils"
+import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
 
 interface IProps {
-  videos: Video[]
+  videos: Video[];
 }
 
-const Home = ({videos}: IProps) => {
-  console.log(videos)
+const Home = ({ videos }: IProps) => {
   return (
-    <div className="flex flex-col gap-10 videos h-full"> 
-      {videos.length ? (
-        videos.map((video: Video)=>(
-          <VideoCard post={video} key={video._id}/>
-        ))
-      ) : (
-        <NoResults text={'No Videos'} />
-      )}
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
     </div>
-  )
-}
+  );
+};
 
 
-export const getServerSideProps = async ()=> {
-  const {data} = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
 
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
   
   return {
-    props: {
-      videos:data
-    }
-  }
-}
-
-
-export default Home
+    props: { videos: response.data },
+  };
+};
+export default Home;
