3ebd890b4cdf893995f39dcad559b4ff023be0d1
ergunDevv <ergncyln2127088@gmail.com>
2023-01-24 16:03:23 +0300

    Comment component backend and functionality added

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -24,6 +24,8 @@ const Detail = ({postDetails}:IProps) => {
   const [post,setPost] = useState(postDetails)
   const [playing,setPlaying] = useState(false);
   const [isVideoMuted,setIsVideoMuted] =useState(false);
+  const [comment,setComment] = useState('');
+  const [isPostingComment,setIsPostingComment] = useState(false);
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const router=useRouter();
@@ -54,6 +56,23 @@ const Detail = ({postDetails}:IProps) => {
       })
       setPost({...post,likes:data.likes});
     }
+  }
+  const addComment =async (e) => {
+    e.preventDefault();
+
+    if(userProfile && comment){
+      setIsPostingComment(true);
+
+      const {data} =await axios.put(`${BASE_URL}/api/post/${post._id}`,{
+        userId: userProfile._id,
+        comment
+      });
+      setPost({...post, comments: data.comments});
+      setComment('');
+      setIsPostingComment(false);
+
+    }
+
   }
 
 
@@ -141,7 +160,11 @@ const Detail = ({postDetails}:IProps) => {
                   )}
               </div>
               <Comments
-              
+                comment={comment}
+                setComment={setComment}
+                addComment={addComment}
+                isPostingComment={isPostingComment}
+                comments={post.comments}
               />
           </div>
       </div>
